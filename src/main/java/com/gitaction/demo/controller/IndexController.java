9826242c5dbcccae5bd7b7e4834f39146a787dff
Y061217 <http1220@naver.com>
2023-06-24 19:05:54 +0900

    빌드 및 ec2로 jar파일 전송 테스트 1번 19:05

--- src/main/java/com/gitaction/demo/controller/IndexController.java
+++ src/main/java/com/gitaction/demo/controller/IndexController.java
@@ -2,13 +2,29 @@ package com.gitaction.demo.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
     @GetMapping("/")
-    public String main(){
+    public String main(Model model){
+        model.addAttribute("data" , "타임리프 엔진 예제입니다.");
+
         return "index";
+    }
+
+    @ResponseBody
+    @GetMapping("/action1")
+    public String action1(){
+        return "action1";
+    }
+
+    @ResponseBody
+    @GetMapping("/action2")
+    public String action2(){
+        return "action2";
     }
 
 }
